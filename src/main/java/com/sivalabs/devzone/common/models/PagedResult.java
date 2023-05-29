a76890c8254c8781d1f5d0b73fc3b57564891508
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-05-29 21:54:24 +0530

    Clean up

---
+++ src/main/java/com/sivalabs/devzone/common/models/PagedResult.java
@@ -0,0 +1,46 @@
+package com.sivalabs.devzone.common.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public record PagedResult<T>(
+        List<T> data,
+        long totalElements,
+        int pageNumber,
+        int totalPages,
+        @JsonProperty("isFirst") boolean isFirst,
+        @JsonProperty("isLast") boolean isLast,
+        @JsonProperty("hasNext") boolean hasNext,
+        @JsonProperty("hasPrevious") boolean hasPrevious) {
+
+    public PagedResult(List<T> data, long totalElements, int pageNumber, int totalPages) {
+        this(
+                data,
+                totalElements,
+                pageNumber,
+                totalPages,
+                pageNumber == 0,
+                pageNumber + 1 == totalPages,
+                pageNumber + 1 < totalPages,
+                pageNumber > 0);
+    }
+
+    public PagedResult(
+            List<T> data,
+            long totalElements,
+            int pageNumber,
+            int totalPages,
+            boolean isFirst,
+            boolean isLast,
+            boolean hasNext,
+            boolean hasPrevious) {
+        this.data = data;
+        this.totalElements = totalElements;
+        this.pageNumber = pageNumber + 1;
+        this.totalPages = totalPages;
+        this.isFirst = isFirst;
+        this.isLast = isLast;
+        this.hasNext = hasNext;
+        this.hasPrevious = hasPrevious;
+    }
+}
